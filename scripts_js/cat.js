3b7d299669c61248cf75b52662144900096eb94a
Alyona Golz <golz.alyona@gmail.com>
2024-01-08 02:01:24 +0400

    Update cat.js

--- scripts_js/cat.js
+++ scripts_js/cat.js
@@ -1,21 +1,38 @@
 function getRandomPosition() {
-  var screenWidth = window.innerWidth - 50;
-  var screenHeight = window.innerHeight - 50;
-  
-  var randomX = Math.floor(Math.random() * screenWidth);
-  var randomY = Math.floor(Math.random() * screenHeight);
-  
-  return { x: randomX, y: randomY };
-}
+        var screenWidth = window.innerWidth - 50;
+        var screenHeight = window.innerHeight - 50;
 
-function updateCatPosition() {
-  var cat = document.getElementById('cat');
-  var newPosition = getRandomPosition();
+        var randomX = Math.floor(Math.random() * screenWidth);
+        var randomY = Math.floor(Math.random() * screenHeight);
 
-  cat.style.left = newPosition.x + 'px';
-  cat.style.top = newPosition.y + 'px';
-}
+        return { x: randomX, y: randomY };
+    }
 
-updateCatPosition();
+    function isOverlap(element1, element2) {
+        var rect1 = element1.getBoundingClientRect();
+        var rect2 = element2.getBoundingClientRect();
 
-setInterval(updateCatPosition, 3000);
+        return (
+            rect1.left < rect2.right &&
+            rect1.right > rect2.left &&
+            rect1.top < rect2.bottom &&
+            rect1.bottom > rect2.top
+        );
+    }
+
+    function updateCatPosition() {
+        var cat = document.getElementById('cat');
+        var content = document.getElementById('content');
+        var newPosition = getRandomPosition();
+
+        while (isOverlap(cat, content)) {
+            newPosition = getRandomPosition();
+        }
+
+        cat.style.left = newPosition.x + 'px';
+        cat.style.top = newPosition.y + 'px';
+    }
+
+    updateCatPosition();
+
+    setInterval(updateCatPosition, 3000);
