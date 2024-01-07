a0b81b076f98d3e11cd17815cea3fcc9ad7d17cd
Alyona Golz <golz.alyona@gmail.com>
2024-01-08 02:57:12 +0400

    Update cat.js

--- scripts_js/cat.js
+++ scripts_js/cat.js
@@ -9,26 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
         return { x: randomX, y: randomY };
     }
 
-    function isOverlap(element1, element2) {
-        var rect1 = element1.getBoundingClientRect();
-        var rect2 = element2.getBoundingClientRect();
-
-        return (
-            rect1.left < rect2.right &&
-            rect1.right > rect2.left &&
-            rect1.top < rect2.bottom &&
-            rect1.bottom > rect2.top
-        );
-    }
-
     function updateCatPosition() {
         var cat = document.getElementById('cat');
-        var content = document.getElementById('content');
         var newPosition = getRandomPosition();
-
-        while (isOverlap(cat, content)) {
-            newPosition = getRandomPosition();
-        }
 
         cat.style.left = newPosition.x + 'px';
         cat.style.top = newPosition.y + 'px';
