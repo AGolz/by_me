422dbfd27719f811f35d8ee8a3341ea9bf431f80
Alyona Golz <golz.alyona@gmail.com>
2024-01-08 01:53:52 +0400

    Create cat.js

---
+++ scripts_js/cat.js
@@ -0,0 +1,21 @@
+function getRandomPosition() {
+  var screenWidth = window.innerWidth - 50;
+  var screenHeight = window.innerHeight - 50;
+  
+  var randomX = Math.floor(Math.random() * screenWidth);
+  var randomY = Math.floor(Math.random() * screenHeight);
+  
+  return { x: randomX, y: randomY };
+}
+
+function updateCatPosition() {
+  var cat = document.getElementById('cat');
+  var newPosition = getRandomPosition();
+
+  cat.style.left = newPosition.x + 'px';
+  cat.style.top = newPosition.y + 'px';
+}
+
+updateCatPosition();
+
+setInterval(updateCatPosition, 3000);
