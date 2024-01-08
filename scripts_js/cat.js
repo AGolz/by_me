d74afb3c49150872ad4192d0d622e6b039381cbf
Alyona Golz <golz.alyona@gmail.com>
2024-01-08 08:36:20 +0400

    Update cat.js

--- scripts_js/cat.js
+++ scripts_js/cat.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var catWidth = 50;
     var rightIndentation = 50;
     var leftIndentation = 90;
-    var appearanceDuration = 5700;
+    var appearanceDuration = 3800;
     var disappearanceInterval = 30000;
 
     function getRandomYPosition() {
