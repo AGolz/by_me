9ae5b8148b344fa0115e8e12c89b103d7f9aed1f
Alyona Golz <golz.alyona@gmail.com>
2024-01-08 04:03:33 +0400

    Update cat.js

--- scripts_js/cat.js
+++ scripts_js/cat.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     var cat = document.getElementById('cat');
-    var screenWidth = window.innerWidth - 50;
     var catWidth = 50;
 
     function getRandomYPosition() {
@@ -10,22 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function updateCatPosition() {
-        var randomY = getRandomYPosition();
-        
-        cat.style.left = '0px';
-        cat.style.top = randomY + 'px';
+        var randomSide = Math.random() < 0.5 ? 'left' : 'right';
+        var screenWidth = window.innerWidth - 50;
 
-        cat.style.transition = 'left 3s linear';
-        cat.style.left = screenWidth - catWidth + 'px';
-
-        // Reset cat's position after the animation
-        setTimeout(function () {
-            cat.style.transition = 'none';
+        if (randomSide === 'left') {
             cat.style.left = '0px';
-            requestAnimationFrame(updateCatPosition);
-        }, 3000);
+        } else {
+            cat.style.left = (screenWidth - catWidth) + 'px';
+        }
+
+        var randomY = getRandomYPosition();
+        cat.style.top = randomY + 'px';
     }
 
     updateCatPosition();
+
+    setInterval(updateCatPosition, 3000);
 });
 
