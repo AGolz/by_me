3c5945b9d6ed687c9dfa87cf0e7368cb59b75c1b
Alyona Golz <golz.alyona@gmail.com>
2024-01-09 04:02:14 +0400

    Update cat.js

--- scripts_js/cat.js
+++ scripts_js/cat.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var rightIndentation = 50;
     var leftIndentation = 90;
     var appearanceDuration = 17100;
-    var disappearanceInterval = 30000;
+    var disappearanceInterval = 60000;
 
     function getRandomYPosition() {
         var screenHeight = window.innerHeight - 50;
