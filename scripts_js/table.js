25502c89e39e21a1ad8faaa8b7fd58842df263ed
Alyona Golz <golz.alyona@gmail.com>
2024-01-17 12:35:29 +0400

    Update table.js

--- scripts_js/table.js
+++ scripts_js/table.js
@@ -1,30 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   showProject('born2beRoot');
 });
 
 function showProject(projectId) {
   var tables = document.querySelectorAll('table');
-
+  
   tables.forEach(function (table) {
-    table.classList.remove('fade-in', 'table-visible');
-    table.classList.add('fade-out');
+    table.classList.remove('table-visible');
+    table.classList.add('table-hidden');
   });
-
+  
   var selectedTable = document.getElementById(projectId);
-  selectedTable.classList.remove('fade-out');
-  selectedTable.classList.add('fade-in', 'table-visible');
-
-  history.pushState(null, null, `#${projectId}`);
+  selectedTable.classList.remove('table-hidden');
+  selectedTable.classList.add('table-visible');
+  
+  setTimeout(function () {
+    tables.forEach(function (table) {
+      if (table.id !== projectId) {
+        table.style.display = 'none';
+      }
+    });
+    
+    selectedTable.style.display = 'table';
+  }, 500);
 }
-
-window.addEventListener('popstate', function () {
-  var currentFragment = window.location.hash.substring(1);
-  showProject(currentFragment);
-});
-
-window.onload = function () {
-  var initialFragment = window.location.hash.substring(1);
-  if (initialFragment) {
-    showProject(initialFragment);
-  }
-};
